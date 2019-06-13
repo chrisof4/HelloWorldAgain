9c8c507a5d83c3dafd0d94d23fe6e8fb33729c8f
Chris Phillips <ckphillips@jackhenry.com>
2019-06-13 16:47:08 -0500

    Modified HelloWorldAgain. First commit.

--- HelloWorldAgain/Program.cs
+++ HelloWorldAgain/Program.cs
@@ -6,7 +6,19 @@ namespace HelloWorldAgain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userName = "";
+            int userAge = 0;
+            int currentYear = 0;
+
+            Console.Write("Please enter your name: ");
+            userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the current year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Hello World! My name is {0} and I am {1} years old. I was born in {2}.", userName, userAge, currentYear - userAge);
         }
     }
 }
